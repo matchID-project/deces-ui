9550b5d904e8b05a0216c270debe7b6217dbfc06
antoinefa <fabien.antoine@m4x.org>
2020-01-27 20:36:17 +0100

    add Layout for easier sep of app and components

---
+++ src/components/views/Layout.svelte
@@ -0,0 +1,12 @@
+<div class="app">
+    <MatchIDHeader/>
+    <SearchHeader/>
+    <Results/>
+</div>
+
+<script>
+	import MatchIDHeader from './MatchIDHeader.svelte';
+	import SearchHeader from './SearchHeader.svelte';
+    import Results from './Results.svelte';
+
+</script>
