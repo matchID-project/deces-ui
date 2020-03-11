36d5f834ea7ea61735cdee40be670b505e210d20
antoinefa <fabien.antoine@m4x.org>
2020-03-11 22:07:37 +0100

    add reusable queries and dateRange query

---
+++ src/components/tools/queries.js
@@ -0,0 +1,65 @@
+export const prefixQuery = (field, value, fuzzy) => {
+    return {
+        prefix: {
+            [field]: value
+        }
+    };
+};
+
+export const matchQuery = (field, value, fuzzy) => {
+    return {
+        match: {
+            [field]: value
+        }
+    }
+};
+
+export const fuzzyTermQuery = (field, value, fuzzy) => {
+    if (fuzzy) {
+        return {
+            bool: {
+                must: [
+                    {
+                        match: {
+                            [field]: {
+                                query: value,
+                                fuzziness: fuzzy
+                            }
+                        }
+                    }
+                ],
+                should: [
+                    {
+                        match: {
+                            [field]: value
+                        }
+                    }
+                ]
+            }
+        };
+    } else {
+        return matchQuery(field, value, false);
+    }
+};
+
+export const dateRangeStringQuery = (field, value, fuzzy) => {
+    console.log('dRSQ',value,Array.isArray(value),value.length);
+    if (Array.isArray(value) && (value.length === 2)) {
+        let min = (value[0] <= value[1]) ? value[0] : value[1];
+        min = min.padEnd(8,'0');
+        let max = (value[0] <= value[1]) ? value[1] : value[0];
+        max = max.padEnd(8,'9');
+        return {
+            range: {
+                [field]: {
+                    gte: min,
+                    lte: max
+                }
+            }
+        };
+    } else if (value.length < 8){
+        return prefixQuery(field, value, false);
+    } else {
+        return fuzzyTermQuery(field, value, fuzzy)
+    }
+};
