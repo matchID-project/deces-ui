3ca45167bdc1831c270424415cc2a633416ab658
antoinefa <fabien.antoine@m4x.org>
2020-10-17 00:07:46 +0200

    dsfr: stronger menu

--- src/components/views/MatchIDHeader.svelte
+++ src/components/views/MatchIDHeader.svelte
@@ -28,33 +28,54 @@
             </div>
             <div class="rf-header__tools">
                 <div class="rf-shortcuts" role="navigation" aria-label="Menu principal">
-                    <ul class="rf-shortcuts__list">
-                        <li class="rf-shortcuts__item">
+                    <ul class="rf-nav__list">
+                        <li
+                            class="rf-nav__item rf-nav__item--hoverable"
+                            class:rf-nav__item--active={($route.path === '/search')}
+                            aria-expanded={expandMenu}
+                            aria-controls="rf-recherche"
+                            on:click|preventDefault={() => {expandMenu = !expandMenu}}
+                        >
                             <span
-                                class="rf-link rf-href rf-fi-change-line rf-link--icon-left"
-                                on:click|preventDefault={() => goToPage('search')}
+                                class="rf-btn"
                             >
                                 recherche
-                                {
-                                    ($route.path === '/search')
-                                        ? ( $advancedSearch ? 'simplifiée' : 'avancée' )
-                                        : ''
-                                }
+                            </span>
+                            <div
+                                class="rf-menu"
+                                id="rf-recherche"
+                            >
+                                <ul class="rf-menu__list">
+                                    {#each searchMenu as item}
+                                        <li
+                                            class="rf-menu__item rf-href"
+                                            class:rf-menu__item--active={($route.path === '/search') && item.isActive}
+                                            on:click|preventDefault={() => goToPage('search',item.mode)}
+                                        >
+                                            <span class="rf-link">
+                                                {item.title}
+                                            </span>
+                                        </li>
+                                    {/each}
+                                </ul>
+                            </div>
+                        </li>
+
+                        <li
+                            class="rf-nav__item"
+                            on:click|preventDefault={() => goToPage('link')}
+                            class:rf-nav__item--active={($route.path === '/link')}
+                        >
+                            <span class="rf-link rf-href rf-fi-db-line rf-link--icon-left">
+                                appariement
                             </span>
                         </li>
-                        <li class="rf-shortcuts__item">
-                            <span
-                                class="rf-link rf-href rf-fi-db-line rf-link--icon-left"
-                                on:click|preventDefault={() => goToPage('link')}
-                            >
-                            appariement
-                            </span>
-                        </li>
-                        <li class="rf-shortcuts__item">
-                            <span
-                                class="rf-link rf-href rf-fi-question-line rf-link--icon-left"
-                                on:click|preventDefault={() => goToPage('about')}
-                            >
+                        <li
+                            class="rf-nav__item"
+                            on:click|preventDefault={() => goToPage('about')}
+                            class:rf-nav__item--active={($route.path === '/about')}
+                        >
+                            <span class="rf-link rf-href rf-fi-question-line rf-link--icon-left">
                                 à propos
                             </span>
                         </li>
@@ -79,37 +100,58 @@
                     on:click|preventDefault={toggleBurger}
                 >
                 </span>
-                <ul class="rf-shortcuts__list">
-                    <li class="rf-shortcuts__item">
-                        <span
-                            class="rf-link rf-href rf-fi-change-line rf-link--icon-left"
-                            on:click|preventDefault={() => goToPage('search')}
+                    <ul class="rf-nav__list">
+                        <li
+                            class="rf-nav__item rf-nav__item--hoverable"
+                            class:rf-nav__item--active={($route.path === '/search')}
+                            aria-expanded={expandMenu}
+                            aria-controls="rf-recherche"
+                            on:click|preventDefault={() => {expandMenu = !expandMenu}}
                         >
-                            recherche
-                            {
-                                ($route.path === '/search')
-                                    ? ( $advancedSearch ? 'simplifiée' : 'avancée' )
-                                    : ''
-                            }
-                        </span>
-                    </li>
-                    <li class="rf-shortcuts__item">
-                        <span
-                            class="rf-link rf-href rf-fi-db-line rf-link--icon-left"
+                            <span
+                                class="rf-btn"
+                            >
+                                recherche
+                            </span>
+                            <div
+                                class="rf-menu"
+                                id="rf-recherche"
+                            >
+                                <ul class="rf-menu__list">
+                                    {#each searchMenu as item}
+                                        <li
+                                            class="rf-menu__item rf-href"
+                                            class:rf-menu__item--active={($route.path === '/search') && item.isActive}
+                                            on:click|preventDefault={() => goToPage('search',item.mode)}
+                                        >
+                                            <span class="rf-link">
+                                                {item.title}
+                                            </span>
+                                        </li>
+                                    {/each}
+                                </ul>
+                            </div>
+                        </li>
+
+                        <li
+                            class="rf-nav__item"
                             on:click|preventDefault={() => goToPage('link')}
+                            class:rf-nav__item--active={($route.path === '/link')}
                         >
-                        appariement
-                        </span>
-                    </li>
-                    <li class="rf-shortcuts__item">
-                        <span
-                            class="rf-link rf-href rf-fi-question-line rf-link--icon-left"
+                            <span class="rf-link rf-href rf-fi-db-line rf-link--icon-left">
+                                appariement
+                            </span>
+                        </li>
+                        <li
+                            class="rf-nav__item"
                             on:click|preventDefault={() => goToPage('about')}
+                            class:rf-nav__item--active={($route.path === '/about')}
                         >
-                            à propos
-                        </span>
-                    </li>
-                </ul>
+                            <span class="rf-link rf-href rf-fi-question-line rf-link--icon-left">
+                                à propos
+                            </span>
+                        </li>
+                    </ul>
             </div>
         </div>
     </div>
@@ -118,55 +160,45 @@
 <script>
     import { themeDnum, advancedSearch, displayMode } from '../tools/stores.js';
     import SearchBox from './SearchBox.svelte';
-    import { toggleAdvancedSearch } from '../tools/search.js';
+    import { toggleAdvancedSearch, enableDisplayMode } from '../tools/search.js';
     import { goTo } from '../tools/routes.js';
     import { route } from '../tools/stores.js';
-
-    import {
-        faAddressCard,
-        faCog,
-        faCoins,
-        faDatabase,
-        faGlobeEurope,
-        faGripLines,
-        faMagic,
-        faPlug,
-        faPlus,
-        faMinus,
-        faSearch,
-        faTable
-    } from '@fortawesome/free-solid-svg-icons';
-
-    import {
-        faFacebook,
-        faTwitter
-    } from '@fortawesome/free-brands-svg-icons';
 
     // import { createEventDispatcher } from 'svelte';
 
     // const dispatch = createEventDispatcher();
 
     let burgerState = false;
+    let expandMenu = false;
     let organization;
+    let searchMenu;
 
-    let aboutMenu = [
-      {
-        url: '/about/service',
-        icon: faSearch,
-        title:'ce service'
-      },
-      {
-        url: '/about/data',
-        icon: faDatabase,
-        title: 'données INSEE'
-      },
-      {
-        url: '/deces/api/v1/docs',
-        icon: faPlug,
-        title:'documentation API'
-      }
+    $: searchMenu = [
+        { title: 'recherche simplifiée',
+          mode: 'simple',
+          isActive: !$advancedSearch
+        },
+        { title: 'recherche avancée',
+          mode: 'advanced',
+          isActive: $advancedSearch
+        },
+        { title: 'affichage simple',
+          mode: 'card',
+          isActive: $displayMode === 'card'
+        },
+        { title: 'affichage complet',
+          mode: 'card-expand',
+          isActive: $displayMode === 'card-expand'
+        },
+        { title: 'affichage tableau',
+          mode: 'table',
+          isActive: $displayMode === 'table'
+        },
+        { title: 'affichage géoraphique',
+          mode: 'geo',
+          isActive: $displayMode === 'geo'
+        }
     ]
-
 
     $: organization = $themeDnum ? 'Ministère<br>de l\'Intérieur' : 'République<br>Française';
 
@@ -174,10 +206,19 @@
         burgerState = !burgerState
     }
 
-    function goToPage(page) {
+    function goToPage(page, mode) {
         burgerState = false;
-        if ((page === 'search') && ($route.path === '/search')) {
-            toggleAdvancedSearch();
+        if ((page === 'search')) {
+            if ($route.path !== '/search') {
+                goTo({path: `/${page}`});
+            }
+            if (mode === 'simple') {
+                toggleAdvancedSearch(false);
+            } else if (mode === 'advanced') {
+                toggleAdvancedSearch(true);
+            } else {
+                enableDisplayMode(mode)
+            }
         } else {
             goTo({path: `/${page}`});
         }
