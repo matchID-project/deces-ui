d26abbd13723bc1a256efb524e73d1e8d766729c
antoinefa <fabien.antoine@m4x.org>
2020-11-12 23:02:05 +0100

    fix missing files

---
+++ src/components/views/Default.svelte
@@ -0,0 +1,19 @@
+<div class="rf-container">
+  <div class="rf-grid-row">
+    <div class="rf-col-12 rf-padding-top-3N">
+      <h2 class="rf-h2 rf-text--center">
+        {title}
+      </h2>
+    </div>
+    <Info/>
+  </div>
+</div>
+
+<script>
+  import Info from './Info.svelte';
+  export let title = undefined;
+</script>
+
+<style>
+
+</style>
