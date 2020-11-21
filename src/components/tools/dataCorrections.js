6eb5390c728c0b2b74911eef2a45f6b79038f4ee
antoinefa <fabien.antoine@m4x.org>
2020-11-21 23:12:13 +0100

    fix id of removal

--- src/components/tools/dataCorrections.js
+++ src/components/tools/dataCorrections.js
@@ -1,5 +1,5 @@
 export const dataCorrections = {
-    "851a1f33fe2f1cde84e79d9e259499b14c5d6cd3": {
+    "f0fba41877734ff8737be32cbb8fee46e6a98e6b": {
       "permalink": "/search?advanced=true&fuzzy=false&view=card-expand&ln=Souche&fn=Bruno%2CEtienne%2CMarie&bd=19%2F11%2F1957&bc=Montpellier&dd=10%2F04%2F2020",
       "change": "cancel",
       "request": "user",
