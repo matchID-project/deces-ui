02700c70765a0ffe02998c1ed185fe21b5fc3fe6
antoinefa <fabien.antoine@m4x.org>
2020-11-30 17:26:44 +0100

    enable subtitle in Default if wanted

--- src/components/views/Default.svelte
+++ src/components/views/Default.svelte
@@ -1,17 +1,30 @@
 <div class="rf-container">
   <div class="rf-grid-row">
     <div class="rf-col-12 rf-padding-top-3N">
-      <h2 class="rf-h2 rf-text--center">
-        {title}
+      <h2 class="rf-text--center">
+        {title || "Non trouvé"}
       </h2>
     </div>
-    <Info/>
+    {#if subtitle}
+      <div class="rf-col-12 rf-padding-bottom-2N">
+        <p class="rf-text--center rf-text--lg">
+          {subtitle}
+        </p>
+      </div>
+    {/if}
+    {#if Info}
+      <svelte:component this={Info}/>
+    {/if}
   </div>
 </div>
 
 <script>
-  import Info from './Info.svelte';
+  let Info;
+  import('./Info.svelte').then(module => {
+    Info = module.default;
+  });
   export let title = undefined;
+  export let subtitle = undefined;
 </script>
 
 <style>
