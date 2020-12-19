22377b2895889bc6df78d921ea9f7da1f505f015
antoinefa <fabien.antoine@m4x.org>
2020-12-19 20:59:53 +0100

    fix false death in dataCorrection

--- src/components/tools/dataCorrections.js
+++ src/components/tools/dataCorrections.js
@@ -1,5 +1,5 @@
 export const dataCorrections = {
-    "f0fba41877734ff8737be32cbb8fee46e6a98e6b": {
+    "5054f8d6c8f7c10615719f4ebdff7f3afcb90a32": {
       "permalink": "/search?advanced=true&fuzzy=false&view=card-expand&ln=Souche&fn=Bruno%2CEtienne%2CMarie&bd=19%2F11%2F1957&bc=Montpellier&dd=10%2F04%2F2020",
       "change": "cancel",
       "request": "user",
