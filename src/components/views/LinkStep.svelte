dae765adae181a0cb93940aba33b8b9d3b2820ee
antoinefa <fabien.antoine@m4x.org>
2021-02-27 20:44:37 +0100

    🎨 Link: prettier colors with rf-callout & scheme

--- src/components/views/LinkStep.svelte
+++ src/components/views/LinkStep.svelte
@@ -1,39 +1,21 @@
 <div
-    class="rf-tile"
+    class="rf-callout rf-padding-top-4N rf-padding-left-2N rf-padding-right-2N rf-padding-bottom-1N"
+    class:rf-fi-play-line={!error && ($linkStep === step)}
+    class:rf-callout--scheme-soft-blue-soft={!error && ($linkStep === step)}
+    class:rf-fi-checkbox-line={!error && (($linkStep > step) || ($linkCompleted))}
+    class:rf-callout--scheme-soft-green-soft={!error && (($linkStep > step) || ($linkCompleted))}
+    class:rf-fi-alert-line={error}
     class:rf-inactive={step > $linkStep}
-    class:rf-success={ (step < $linkStep) || $linkCompleted }
+    class:rf-fi-lock-fill={step > $linkStep}
     class:rf-text--error={error}
+    style="postition: relative;"
 >
-    <div
-        class="rf-tile__body"
-    >
-        <h4
-            class="rf-tile__title"
-            style="position: relative"
-            class:rf-text--error={error}
-        >
-            <span class="rf-margin-right-3N">Étape {step}. {title}</span>
-            {#if error}
-                <span
-                    class="rf-fi-alert-line rf-fi--lg"
-                    style="position: absolute;top: 0px;right:0px;"
-                ></span>
-            {:else if ($linkStep > step) || ($linkCompleted)}
-                <span
-                    class="rf-fi-checkbox-line rf-fi--lg"
-                    style="position: absolute;top: 0px;right:0px;"
-                ></span>
-            {:else if ($linkStep === step)}
-                <span
-                    class="rf-fi-play-line rf-fi--lg"
-                    style="position: absolute;top: 0px;right:0px;"
-                ></span>
-            {/if}
-        </h4>
-        <p class="rf-tile__desc rf-text--center" style="overflow:hidden;text-overflow:ellipsis;">
-            {@html label}
-        </p>
-    </div>
+    <p style="position: absolute;top: 1rem;left: 3rem;margin: 0">
+        <strong>Étape {step}. {title}</strong>
+    </p>
+    <p class="rf-text--center" style="overflow:hidden;text-overflow:ellipsis;">
+        {@html label}
+    </p>
 </div>
 <script>
     import { linkStep, linkCompleted } from '../tools/stores.js';
