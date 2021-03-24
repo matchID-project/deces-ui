a45e4fbd11444839fdf772c1b61897146aa8d533
antoinefa <fabien.antoine@m4x.org>
2021-03-24 01:47:00 +0100

    ✨ quick n dirty job list view, no action

--- src/components/tools/routes.js
+++ src/components/tools/routes.js
@@ -2,6 +2,7 @@ import Default from '../views/Default.svelte';
 import Search from '../views/Search.svelte';
 import ShowId from '../views/ShowId.svelte';
 import Link from '../views/Link.svelte';
+import LinkJobs from '../views/LinkJobs.svelte';
 import Stats from '../views/Stats.svelte';
 
 import {
@@ -38,6 +39,7 @@ export const routes = {
     },
     '/about': { component: Default, props: {title: 'À propos'}, title: 'À propos', desc: 'À propos de matchID: conditions légales données et code opensource, foire aux questions, documentation de l\'API, statistiques de consultation, ...'},
     '/link': { component: Link, title: 'Appariement', desc: 'matchID permet l\'appariement de votre fichier client, jusque 1 million d\'enregistrements, au fichier des décès de l`INSEE' },
+    '/jobs': { component: LinkJobs, title: 'Traitements', desc: 'page d\'administration des traitements' },
     '/stats': { component: Stats, title: 'statistiques de fréquentation', desc: 'statistiques de consultation de deces.matchid.io: visiteurs uniques, appels API, ... historique depuis début 2020' },
     '/notFound': { component: Default, props: { title: 'Pas de page à l\'adresse indiquée'}, title: '404 - Pas de page à l\'adresse indiquée', desc: '404' },
 };
---
+++ src/components/views/LinkJobs.svelte
@@ -0,0 +1,86 @@
+<div class="rf-container">
+    <div class="rf-grid-row">
+        <div class="rf-col-12 rf-padding-2N rf-text--center">
+            <p>
+                <strong> Liste des traitements
+                </strong>
+            </p>
+            {#if jobs.length}
+                <table class="rf-table rf-table--narrow rf-table--striped">
+                    <tbody>
+                        <tr>
+                            <td><strong>id</strong></td>
+                            <td><strong>date</strong></td>
+                            <td><strong>lignes</strong></td>
+                            <td><strong>statut</strong></td>
+                        </tr>
+                        {#each jobs as job}
+                            <tr>
+                                <td>{job.id.substring(0,20) + '...'}</td>
+                                <td>{job.date.toISOString()}</td>
+                                <td>{job.rows}</td>
+                                <td>{job.status}</td>
+                            </tr>
+                        {/each}
+                    </tbody>
+                </table>
+            {:else}
+                Aucun job
+            {/if}
+        </div>
+    </div>
+</div>
+<script>
+    import { onMount } from 'svelte';
+    import { tweened } from 'svelte/motion';
+	import { sineInOut } from 'svelte/easing';
+    import axios from 'axios';
+
+    import { accessToken } from '../tools/stores.js';
+    let jobs = [];
+
+    const getJobsData = async () => {
+        let response = await fetch('__BACKEND_PROXY_PATH__/queue/jobs', {headers: {Authorization: `Bearer ${$accessToken}`}});
+        const types = Object.keys(await response.json());
+        const tmpJobs = [];
+        await Promise.all(types.map(async (type) => {
+            response = await fetch(`__BACKEND_PROXY_PATH__/queue/jobs/${type}`, {headers: {Authorization: `Bearer ${$accessToken}`}});
+            const list = (await response.json()).jobs || [];
+            list.forEach(j => {tmpJobs.push({rows: j.data.totalRows, id: j.data.randomKey, date: new Date(j.options.timestamp), status: j.status})});
+        }));
+        jobs = tmpJobs.sort((a,b) => (a.date >= b.date));
+    }
+
+    $: if ($accessToken) {
+        getJobsData();
+    }
+
+</script>
+
+<style>
+
+    .progress.is-info::-moz-progress-bar {
+        background-color: var(--bf500);
+    }
+
+    .progress::-moz-progress-bar {
+        background-color: var(--g500);
+    }
+
+    .progress.is-info::-webkit-progress-value {
+        background-color: var(--bf500);
+    }
+
+    .progress {
+        -moz-appearance: none;
+        -webkit-appearance: none;
+        border: none;
+        border-radius: 290486px;
+        display: block;
+        height: 1rem;
+        overflow: hidden;
+        padding: 0;
+        width: 100%;
+    }
+
+</style>
