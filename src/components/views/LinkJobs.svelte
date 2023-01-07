ed814b98aa646e443c6fc20dbfac889d416203ea
antoinefa <fabien.antoine@m4x.org>
2023-01-06 22:44:01 -0500

    ✨ adds perf monitoring in jobs

--- src/components/views/LinkJobs.svelte
+++ src/components/views/LinkJobs.svelte
@@ -6,15 +6,35 @@
             <div class="rf-col-xl-8 rf-col-lg-8 rf-col-md-12 rf-col-sm-12 rf-col-xs-12">
                 <div class="rf-container-fluid">
                     <div class="rf-grid-row rf-grid-row--gutters">
-                        <div class="rf-col-4">
-                            <StatsTile number={jobs.filter(j => j.status === 'completed').length} label="Nombre de traitements"/>
+                        <div class="rf-col-3">
+                            <StatsTile
+                                number={jobs.filter(j => j.status === 'completed').length}
+                                label="Traitements"/>
                         </div>
-                        <div class="rf-col-4">
-                            <StatsTile number={jobs.filter(j => j.status === 'completed').length
-                                && jobs.filter(j => j.status === 'completed').map(j =>j.rows).reduce((a,b) => a+b)} label="Appariements"/>
+                        <div class="rf-col-3">
+                            <StatsTile
+                                number={
+                                    jobs.filter(j => j.status === 'completed').length &&
+                                    jobs.filter(j => j.status === 'completed').map(j =>j.rows).reduce((a,b) => a+b)
+                                }
+                                label="Appariements"/>
                         </div>
-                        <div class="rf-col-4">
-                            <StatsTile number={jobs.filter(j => j.status === 'failed').length} label="Traitements en échec"/>
+                        <div class="rf-col-3">
+                            <StatsTile
+                                number={jobs.filter(j => j.status === 'failed').length}
+                                label="Échecs"/>
+                        </div>
+                        <div class="rf-col-3">
+                            <StatsTile
+                                number={
+                                    jobs.filter(j => j.status === 'completed').length &&
+                                    Math.floor(
+                                        jobs.filter(j => j.status === 'completed').map(j =>j.rows).reduce((a,b) => a+b)
+                                        /
+                                        jobs.filter(j => j.status === 'completed').map(j =>j.delay).reduce((a,b) => a+b)
+                                    )
+                                }
+                                label="Lignes / Seconde"/>
                         </div>
                     </div>
                 </div>
@@ -33,33 +53,41 @@
                         <table class="rf-table rf-table--narrow rf-table--striped">
                             <thead>
                                 <tr>
-                                    <th>id</th>
                                     <th>date</th>
-                                    <th>progrès(%)</th>
-                                    <th>lignes</th>
+                                    <th>id</th>
                                     <th>statut</th>
+                                    <th>lignes</th>
+                                    <th>performance</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 {#each jobs as job, idx}
                                     <tr>
-                                        <td>{job.id.substring(0,20) + '...'}</td>
                                         <td>{job.date}</td>
-                                        <td>{job.progress}</td>
-                                        <td>{job.rows}</td>
+                                        <td>{job.id.substring(0,10) + '...'}</td>
                                         <td>
                                             <div style="display: flex;align-items:center">
-                                                {statusLabel[job.status] || job.status}
                                                 {#if job.status == 'active'}
+                                                    {job.progress}%
                                                     <span
                                                         title="arrêter"
-                                                        class="rf-margin-left-4px"
                                                         on:click|preventDefault={() => deleteJob(job, idx)}
+                                                        on:keypress|preventDefault={() => deleteJob(job, idx)}
                                                     >
-                                                    <Icon icon="ri:delete-bin-line" class="rf-color--rm"/>
+                                                        <Icon icon="ri:delete-bin-line" class="rf-color--rm"/>
                                                     </span>
+                                                {:else}
+                                                    {statusLabel[job.status] || job.status}
                                                 {/if}
                                             </div>
+                                        </td>
+                                        <td>{job.rows}</td>
+                                        <td>
+                                            {#if job.performance}
+                                                {job.performance} ligne/s
+                                            {:else}
+                                                N/A
+                                            {/if}
                                         </td>
                                     </tr>
                                 {/each}
@@ -106,10 +134,16 @@
         const tmpJobs = [];
         const list = (await response.json()).jobs || [];
         list.forEach(j => {
+            const delay = (
+                (j.finishedOn ? j.finishedOn : Math.floor(Date.now()))
+                - j.processedOn
+            ) / 1000;
             tmpJobs.push({
                 rows: j.data.totalRows,
                 id: j.id, date: j.timestamp,
                 status: j.status,
+                delay: delay,
+                performance: j.processedOn && Math.floor(j.data.totalRows / delay),
                 progress: j.status && j.status === "completed" ? "100" : j.progress && j.progress.percentage ? Math.round(j.progress.percentage) : 0
             })});
         jobs = tmpJobs.sort((a,b) => (b.date - a.date)).map(j => {
