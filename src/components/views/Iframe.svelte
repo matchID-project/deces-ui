46baa785d18eec43b91aa71cc9305b641e5e6aaf
antoinefa <fabien.antoine@m4x.org>
2023-07-23 21:59:20 -0400

    Reuse matchid.io link-api page for scoring explanation, and full algorithm

--- src/components/views/Iframe.svelte
+++ src/components/views/Iframe.svelte
@@ -1,9 +1,10 @@
 <div style="overflow: hidden">
-    <iframe bind:this={iframe} title="documentation de l'api" {src} style="margin-top:-330px;width: 100%;height: 2200px;border-width: 0px;"></iframe>
+    <iframe bind:this={iframe} title="documentation de l'api" {src} {style}></iframe>
 </div>
 
 <script>
     import { onMount } from 'svelte';
+    export let style =  "margin-top:-330px;width: 100%;height: 2200px;border-width: 0px;";
     export let src = undefined;
     let iframe;
 
